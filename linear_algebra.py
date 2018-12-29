0949aa17c54f1c3f6b08c721e9b428c1e6678f91
Dmitry <dmitrygboyko@gmail.com>
2018-12-29 19:35:08 +1100

    Add scalar_multiply file

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,3 +1,5 @@
+from functools import *
+
 def vector_add(v, w):
     return [v_i + w_i for v_i, w_i in zip(v, w)]
 
@@ -7,7 +9,8 @@ def vector_subtract(v, w):
 
 
 def vector_sum(vectors):
-    result = vectors[0]
-    for vector in vectors[1:]:
-        result = vector_add(result, vector)
-    return result
+    return reduce(vector_add, vectors)
+
+
+def scalar_multiply(c, v):
+    return [c*v_i for v_i in v]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from linear_algebra import *
 
-result = vector_subtract([2, 3], [5, 6])
+result = vector_sum([[3, 4], [2, 3], [5, 6]])
 print(result)
