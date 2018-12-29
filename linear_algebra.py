aa98e6b909e99964fb401e9e82ab44a69b934aec
Dmitry <dmitrygboyko@gmail.com>
2018-12-29 19:29:04 +1100

    Add main.py to the project

--- linear_algebra.py
+++ linear_algebra.py
@@ -2,7 +2,7 @@ def vector_add(v, w):
     return [v_i + w_i for v_i, w_i in zip(v, w)]
 
 
-def vector_subtruct(v, w):
+def vector_subtract(v, w):
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 
 
