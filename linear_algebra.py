beb9c5f67fefa3f5ad2fe328b49958fb1196ddd9
Dmitry <dmitrygboyko@gmail.com>
2018-12-30 16:04:19 +1100

    Add distance and magnitude function

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,4 +1,6 @@
 from functools import *
+import math
+
 
 def vector_add(v, w):
     return [v_i + w_i for v_i, w_i in zip(v, w)]
@@ -14,3 +16,29 @@ def vector_sum(vectors):
 
 def scalar_multiply(c, v):
     return [c*v_i for v_i in v]
+
+
+def vector_mean(vectors):
+    length = len(vectors)
+    return scalar_multiply(1/length, vector_sum(vectors))
+
+
+def dot(v, w):
+    return sum(v_i*w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
+
+
+def squared_distance(v, w):
+    return sum_of_squares(vector_subtract(v, w))
+
+
+def distance(v, w):
+    return magnitude(vector_subtract(v, w))
+
