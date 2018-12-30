00165e33f44f9974ef050bc1db3db9898be3dbb2
Dmitry <dmitrygboyko@gmail.com>
2018-12-30 19:50:53 +1100

    Add mean and medium

--- linear_algebra.py
+++ linear_algebra.py
@@ -42,3 +42,46 @@ def squared_distance(v, w):
 def distance(v, w):
     return magnitude(vector_subtract(v, w))
 
+
+def shape(A):
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
+
+
+def get_row(A, i):
+    return A[i]
+
+
+def get_column(A, j):
+    return [A_i[j] for A_i in A]
+
+
+def make_matrix(num_rows, num_cols, entry_fn):
+    return [[entry_fn(i, j)
+            for j in range(num_cols)]
+            for i in range(num_rows)]
+
+
+def is_diagonal(i, j):
+    return 1 if i == j else 0
+
+
+def mean(v):
+    return sum(v)/len(v)
+
+
+def median(v):
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n//2
+
+    if n % 2 == 1:
+        return sorted_v[midpoint]
+    else:
+        lo = midpoint - 1
+        hi = midpoint
+        return (sorted_v[lo] + sorted_v[hi]) / 2
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from linear_algebra import *
+from collections import *
 
-result = vector_sum([[3, 4], [2, 3], [5, 6]])
-print(result)
+num_friends = [100, 49, 41, 43, 40, 25]
+print(median(num_friends))
