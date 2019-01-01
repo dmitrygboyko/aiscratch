6d0aa2540e7aaab24e2cfe66644e4c8161a0025a
Dmitry <dmitrygboyko@gmail.com>
2019-01-01 16:43:25 +1100

    Add Covariance and Correlation

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,4 +1,5 @@
 from functools import *
+from collections import *
 import math
 
 
@@ -84,4 +85,50 @@ def median(v):
         return (sorted_v[lo] + sorted_v[hi]) / 2
 
 
+def quantile(x, p):
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+
+def mode(x):
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items() if count == max_count]
+
+
+def data_range(x):
+    return max(x) - min(x)
+
+
+def de_mean(x):
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+
+def variance(x):
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+
+def interquartile_range(x):
+    return quantile(x, 0.75) - quantile(x, 0.25)
+
+
+def covariance(x, y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n - 1)
+
+
+def correlation(x, y):
+    stddev_x = standard_deviation(x)
+    stddev_y = standard_deviation(y)
+    if stddev_x > 0 and stddev_y > 0:
+        return covariance(x, y) / stddev_x / stddev_y
+    else:
+        return 0
 
